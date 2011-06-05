5eeaaa54d47d36e7348aff857ea1f4d661fb8b05
Bozydar Sobczak <bozysob@gmail.com>
2011-06-05 19:32:09 +0200

    imported patch StructureRefactoring

---
+++ functional-rogue/LevelGeneration.fs
@@ -0,0 +1,43 @@
+﻿module LevelGeneration
+
+open System.Drawing
+open Board
+
+let minRoomSize = 3
+
+   
+let isOverlapping room rooms =    
+
+    let isOverlapping2 (room1: Room) (room2: Room) =         
+        room1.Rectangle.IntersectsWith room2.Rectangle
+
+    List.exists (isOverlapping2 room) rooms
+
+let rec generateRooms rooms =
+    seq {                        
+        let rec newRoom'() = 
+            let newRoom = 
+                let posX = rnd (boardWidth - minRoomSize)
+                let posY = rnd (boardHeight - minRoomSize)
+                let width = rnd2 minRoomSize (boardWidth - posX)
+                let height = rnd2 minRoomSize (boardHeight - posY)
+                new Room(new Rectangle(posX, posY, width, height))
+            if isOverlapping newRoom rooms 
+            then
+                newRoom'()
+            else
+                newRoom
+        let newRoom = newRoom'()
+        yield newRoom
+        yield! generateRooms (newRoom::rooms)
+    }
+
+let generateLevel: Board = 
+    let mutable board = Array2D.create boardWidth boardHeight {Place.EmptyPlace with Tile = Tile.Floor}
+    let rooms = (generateRooms []) |> Seq.take 4 |> Seq.toList
+    let rec addRooms rooms board =
+        match rooms with
+        | [] -> board
+        | item::t -> addRooms t <| (item :> IModifier).Modify board 
+    addRooms rooms board
+
--- functional-rogue/Utils.fs
+++ functional-rogue/Utils.fs
@@ -1,12 +1,20 @@
 ﻿[<AutoOpen>]
 module Utils
 
+open System
 open Microsoft.FSharp.Reflection
 
-let unionRepr (x:'a) =    
-    match FSharpValue.GetUnionFields(x, typeof<'a>) with
-    | case, _ -> case.Name
-
 let repr (x:'a) =
+    let unionRepr (x:'a) =    
+        match FSharpValue.GetUnionFields(x, typeof<'a>) with
+        | case, _ -> case.Name
+
     if FSharpType.IsUnion(typeof<'a>) then unionRepr x
     else x.ToString()
+
+let private r = new Random()
+let rnd max = 
+    r.Next(max)
+
+let rnd2 min max = 
+    r.Next(min, max)
