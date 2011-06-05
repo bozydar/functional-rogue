19cf63cfe2a3aba503f7f8aced074f0be5907f99
Bozydar Sobczak <bozysob@gmail.com>
2011-06-05 18:04:07 +0200

    imported patch NoSideEffects

--- functional-rogue/Game.fs
+++ functional-rogue/Game.fs
@@ -4,8 +4,6 @@ open System
 open System.Drawing
 open Log
 
-let boardHeight = 24
-let boardWidth = 79
 let minRoomSize = 3
 
 type Tile =
@@ -14,10 +12,69 @@ type Tile =
     | Avatar
     | None
 
-type Board = Tile[,]
+type Item = 
+    | Sword
+    | Vand
 
-let (@) (board: Board) (point: Point) =
-    Array2D.get board point.X point.Y
+type CharacterType = 
+    | Avatar
+    | Monster
+    | NPC
+
+type Character = {
+    Type: CharacterType
+}    
+
+type Place = {
+    Tile: Tile; 
+    Items: Item list;
+    Character: Character option;    
+} with
+    static member EmptyPlace = 
+            {Tile = Tile.None; Items = []; Character = Option.None }
+
+module Board =
+
+    let boardHeight = 24
+    let boardWidth = 79
+
+    type Board = Place[,]
+                    
+    let get (board: Board) (point: Point) = Array2D.get board point.X point.Y
+
+    let set (point: Point) (value: Place) (board: Board) : Board =
+        let result = Array2D.copy board 
+        Array2D.set result point.X point.Y value
+        result
+
+    let places (board: Board) = 
+        seq {
+            for x = 0 to boardWidth - 1 do
+                for y = 0 to boardHeight - 1 do
+                    let item = Array2D.get board x y
+                    yield (new Point(x, y), item)
+        }
+
+    let moveCharacter (character: Character) (newPosition: Point) (board: Board) =
+        match Seq.tryFind (fun (point, place) -> 
+            match place.Character with 
+            | Some(character1) -> character1 = character
+            | _ -> false) (places board) with        
+        | Some((oldPosition, oldPlace)) ->             
+            let newPlace = { oldPlace with Character = (get board oldPosition ).Character }
+            board 
+            |> set newPosition newPlace 
+            |> set oldPosition { oldPlace with Character = Option.None }
+        | _ ->             
+            let oldPlace = get board newPosition
+            let newPlace = { oldPlace with Character = Some character }
+            board
+            |> set newPosition newPlace
+
+    let emptyBoard : Board = Array2D.create boardWidth boardHeight Place.EmptyPlace
+        
+open Board
+
 
 type IModifier =
     abstract member Modify: Board -> Board
@@ -27,13 +84,15 @@ type Room(rect: Rectangle) =
         if width < 1 then invalidArg "width" "Is zero"
         if height < 1 then invalidArg "height" "Is zero"
 
-        let result = Array2D.create width height Tile.Floor
+        let wall = {Place.EmptyPlace with Tile = Tile.Wall}
+        let floor = {Place.EmptyPlace with Tile = Tile.Floor}
+        let result = Array2D.create width height floor
         for x = 0 to width - 1 do 
-            Array2D.set result x 0 Tile.Wall
-            Array2D.set result x (height - 1) Tile.Wall
+            Array2D.set result x 0 wall
+            Array2D.set result x (height - 1) wall
         for y = 0 to height - 1 do 
-            Array2D.set result 0 y Tile.Wall
-            Array2D.set result (width - 1) y Tile.Wall
+            Array2D.set result 0 y wall
+            Array2D.set result (width - 1) y wall
         result
 
     member this.Rectangle
@@ -53,21 +112,31 @@ type Room(rect: Rectangle) =
 let printBoard (oldBoard: Board) (newBoard: Board) = 
     
     let compareBoard() = seq {
-        let height = Array2D.length1 oldBoard
-        let width = Array2D.length2 oldBoard         
-        for y = 0 to width - 1 do
-            for x = 0 to height - 1 do        
+        let height = boardHeight - 1
+        let width = boardWidth - 1
+        for x = 0 to width do
+            for y = 0 to height do        
                 let newItem = newBoard.[x, y]
                 if not (oldBoard.[x, y] = newItem) then 
                     yield (x, y, newItem)
     }
+
     let printItem = fun item -> 
-        let char = 
-            match item with
-            | Wall ->  "#"
-            | Floor -> "."
-            | Avatar -> "@"
-            | None -> " "
+        let char =             
+            match item.Character with
+            | Some(character1) -> 
+                match character1.Type with
+                | Avatar -> "@"
+                | Monster -> "s"
+                | NPC -> "P"
+            | _ -> 
+                match item.Items with
+                | h::_ -> "i"
+                | _ -> 
+                    match item.Tile with
+                    | Wall ->  "#"
+                    | Floor -> "."
+                    | _ -> " "
         Console.Write char
     for x, y, item in compareBoard() do        
         Console.SetCursorPosition(x, y)
@@ -107,7 +176,7 @@ let rec generateRooms rooms =
     }
 
 let generateLevel: Board = 
-    let mutable board = Array2D.create boardWidth boardHeight Tile.Floor            
+    let mutable board = Array2D.create boardWidth boardHeight {Place.EmptyPlace with Tile = Tile.Floor}
     let rooms = (generateRooms []) |> Seq.take 4 |> Seq.toList
     for item in rooms do        
         board <- (item :> IModifier).Modify board 
@@ -118,6 +187,7 @@ type Command =
     | Down
     | Left
     | Right
+    | Wait
     | Quit
     | Unknown
 
@@ -127,29 +197,23 @@ let commandToSize command =
     | Down -> new Size(0, 1)
     | Left -> new Size(-1, 0)
     | Right -> new Size(1, 0)
-    | _ -> invalidArg "command" ("bad command " + (repr command))
+    | _ -> new Size(0, 0)
 
-type Avatar(board: Board, position: Point, ?previous: Avatar) = 
-    let putOnTile = board @ position
-    interface IModifier with
-        member this.Modify board =            
-            Array2D.set board position.X position.Y Tile.Avatar
-            if previous.IsSome then Array2D.set board previous.Value.Position.X previous.Value.Position.Y putOnTile            
-            board
+let moveCharacter character command board = 
+    let position, _ =  Seq.find (fun (_, place) -> place.Character = Some character) <| places board
 
-    member this.Move command =
-        let size = commandToSize command            
-        match board @ (position + commandToSize command) with
-            | Wall -> this
-            | _ -> new Avatar(board, position + size, this)
+    let move = commandToSize command
+    let newPosition = position + move
+    let newPlace = get board newPosition
     
-    member this.Position 
-        with get() = position
+    match newPlace.Tile with 
+    | Wall -> board
+    | _ ->         
+        board |> moveCharacter character newPosition
 
 type State = {
-    Board: Board; Avatar: Avatar
+    Board: Board
 }
-
 
 
 let mainLoop() =
@@ -157,13 +221,14 @@ let mainLoop() =
         let nextTurn command = 
             let last = states.Head
             // Something... Something...
-            let avatar = last.Avatar.Move command
-            let board = last.Board.Clone() :?> Board |> (avatar :> IModifier).Modify
-            {Board = board; Avatar = avatar}
+            //let avatar = last.Avatar.Move command
+            let board = 
+                last.Board  
+                |> moveCharacter {Type = Avatar} command
+            {Board = board}
 
-        // problem is that new state is bitored by new state and old avatar is not cleared
-        // consider using events for each of state elements to trace all changes and indicate them on board
-        states.Head.Board |> if printAll then printBoard (Array2D.create boardWidth boardHeight Tile.None) else printBoard states.[1].Board
+        states.Head.Board 
+        |> if printAll then printBoard Board.emptyBoard else printBoard states.[1].Board
 
         let char = System.Console.ReadKey(true)        
         
@@ -173,18 +238,21 @@ let mainLoop() =
             | ConsoleKey.DownArrow -> Down            
             | ConsoleKey.LeftArrow -> Left            
             | ConsoleKey.RightArrow -> Right
+            | ConsoleKey.W -> Wait
             | ConsoleKey.Escape -> Quit
             | _ -> Unknown                        
         
         match command with
         | Quit -> ()
         | Unknown -> loop false states
-        | Up | Down | Left | Right -> loop false ((nextTurn command) :: states)
+        | Up | Down | Left | Right | Wait -> loop false ((nextTurn command) :: states)
 
-    let board = generateLevel
+    let board = 
+        generateLevel 
+        |> Board.moveCharacter {Type = CharacterType.Avatar} (new Point(1, 1))
+
     let entryState = {         
         Board = board; 
-        Avatar = new Avatar(board, new Point(1, 1)) 
     }
     loop true [entryState]
 
