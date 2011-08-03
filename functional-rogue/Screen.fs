13d2a580e5844946287cc0b2030c1f1782b563d9
Bozydar Sobczak <bozysob@gmail.com>
2011-08-03 19:28:29 +0200

    Reformat

--- functional-rogue/Screen.fs
+++ functional-rogue/Screen.fs
@@ -31,23 +31,24 @@ let private screenWritter () =
     let writeBoard (board: Board) (boardFramePosition: Point) sightRadius (screen: screen)  = 
         let toTextel item =  
             if item.WasSeen then
-                let result = match item.Character with
-                                | Some(character1) -> 
-                                    match character1.Type with
-                                    | Avatar -> {Char = '@'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
-                                    | Monster -> {Char = 's'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Red}
-                                    | NPC -> {Char = 'P'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.White}
-                                | _ -> 
-                                    match item.Items with
-                                    | h::_ -> 
-                                           match h with 
-                                           | Gold(value) -> {Char = '$'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
-                                           | _ -> {Char = 'i'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
-                                    | _ -> 
-                                        match item.Tile with
-                                        | Wall ->  {Char = '#'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
-                                        | Floor -> {Char = '.'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
-                                        | _ -> empty
+                let result = 
+                    match item.Character with
+                    | Some(character1) -> 
+                        match character1.Type with
+                        | Avatar -> {Char = '@'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
+                        | Monster -> {Char = 's'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Red}
+                        | NPC -> {Char = 'P'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.White}
+                    | _ -> 
+                        match item.Items with
+                        | h::_ -> 
+                                match h with 
+                                | Gold(value) -> {Char = '$'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
+                                | _ -> {Char = 'i'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
+                        | _ -> 
+                            match item.Tile with
+                            | Wall ->  {Char = '#'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
+                            | Floor -> {Char = '.'; FGColor = ConsoleColor.White; BGColor = ConsoleColor.Black}
+                            | _ -> empty
                 if not item.IsSeen then {result with FGColor = ConsoleColor.DarkGray } else result
             else empty
         for x in 0..boardFrame.X - 1 do
@@ -108,16 +109,16 @@ let private screenWritter () =
                                       |> writeStats state
                                   refreshScreen screen newScreen
                                   return! loop newScreen                        
-            | ShowMainMenu(reply) ->    let newScreen = 
-                                            screen
-                                            |> Array2D.copy
-                                            |> cleanScreen
-                                            |> writeString (point 1 1) "What is your name?"
-                                        refreshScreen screen newScreen
-                                        Console.SetCursorPosition(1, 2)
-                                        let name = Console.ReadLine()
-                                        reply.Reply({Name = name})
-                                        return! loop newScreen  
+            | ShowMainMenu(reply) -> let newScreen = 
+                                         screen
+                                         |> Array2D.copy
+                                         |> cleanScreen
+                                         |> writeString (point 1 1) "What is your name?"
+                                     refreshScreen screen newScreen
+                                     Console.SetCursorPosition(1, 2)
+                                     let name = Console.ReadLine()
+                                     reply.Reply({Name = name})
+                                     return! loop newScreen  
         }
         loop <| Array2D.create screenSize.X screenSize.Y empty
     )
