369c7b3d5669a33976e26b087b9ae1660a9d8be9
Jakub Rusilko <brodaty.bosman@gmail.com>
2011-08-12 11:11:08 +0200

    added random monster movement - proof of concept
    --HG--
    branch : adding-monsters

--- functional-rogue/AI.fs
+++ functional-rogue/AI.fs
@@ -2,6 +2,26 @@
 
 open State
 open Monsters
+open Board
+open System.Drawing
+
+let performRandomMovement (monsterPlace: (Point*Place)) (state:State) : State =
+    let mutable possibleNewLocations = []
+    let x = (fst monsterPlace).X
+    let y = (fst monsterPlace).Y
+    for tmpx in (max (x - 1) 0)..(min (x + 1) (boardWidth - 1)) do
+        for tmpy in (max (y - 1) 0)..(min (y + 1) (boardHeight - 1)) do
+            if (not(tmpx = x && tmpy = y) && not(isObstacle state.Board (Point(tmpx,tmpy)))) then
+                possibleNewLocations <- possibleNewLocations @ [Point(tmpx,tmpy)]
+    let resultState = { state with Board = state.Board |> Board.moveCharacter (snd monsterPlace).Character.Value (possibleNewLocations.[rnd possibleNewLocations.Length]) }
+    resultState
 
 let handleMonsters (state: State) : State =
-    state
+    let rec recursivelyHandleMonstersSequence (monsterPlaces: (Point*Place) list) (state:State) : State =
+        match monsterPlaces with
+        | head :: tail -> recursivelyHandleMonstersSequence tail (state |> performRandomMovement head)
+        | [] -> state
+
+    let allMonsterPlaces = monsterPlaces state.Board
+    //TO DO: here sort them by initiative
+    state |> recursivelyHandleMonstersSequence allMonsterPlaces
