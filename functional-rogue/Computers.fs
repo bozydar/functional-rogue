800c1a08b7c88476fb59520e851c468c4f102ca2
Jakub Rusilko <brodaty.bosman@gmail.com>
2011-09-07 14:47:04 +0200

    added computers.fs
    --HG--
    branch : computers

---
+++ functional-rogue/Computers.fs
@@ -0,0 +1,26 @@
+﻿module Computers
+
+open System
+open State
+open Screen
+
+let operateComputer state =
+    let createDisplayContent =
+        let header = "Welcome"
+        let esc = "Hit Esc to exit"
+        [header] @ [""] @ [esc]
+
+    let rec loop (state : State) =
+        //setCursorPositionOnBoard current state
+        let keyInfo = System.Console.ReadKey(true)
+        match keyInfo with 
+        | Keys [ConsoleKey.UpArrow; '8'] -> loop (state) //Up
+        | Keys [ConsoleKey.DownArrow; '2'] -> loop (state) //Down        
+        | Keys [ConsoleKey.LeftArrow; '4'] -> loop (state) //Left            
+        | Keys [ConsoleKey.RightArrow; '6'] -> loop (state) //Right
+        | Key ConsoleKey.Escape -> state
+        | _ ->
+            displayComputerScreen createDisplayContent
+            loop state
+    displayComputerScreen createDisplayContent
+    loop state 
