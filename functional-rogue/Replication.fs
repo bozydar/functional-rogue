362bcd3343a35d8dd76863680cd64a5c6f0a4b5d
Jakub Rusilko <brodaty.bosman@gmail.com>
2011-09-19 18:45:27 +0200

    included replication code file
    --HG--
    branch : computers

---
+++ functional-rogue/Replication.fs
@@ -0,0 +1,21 @@
+﻿module Replication
+
+open Items
+
+type ReplicationRecipe = {
+    Name : string
+    ResultItem : PredefinedItems
+    RequiredResources : RequiredResources
+}
+and RequiredResources = {
+    Iron : int
+    Gold : int
+    Uranium : int
+}
+
+let defaultRequiredResources = {
+    Iron = 0; Gold = 0; Uranium = 0
+}
+
+let allRecipes =
+    [{ Name = "Combat Knife"; ResultItem = CombatKnife; RequiredResources = { defaultRequiredResources with Iron = 2 } }]
