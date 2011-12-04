19dd3087aaa9a151c8fd9673a190a766cbf37405
Bozydar Sobczak <bozysob@gmail.com>
2011-12-04 15:05:01 +0100

    Screen: Dialog added

---
+++ functional-rogue/Dialog.fs
@@ -0,0 +1,23 @@
+﻿[<RequireQualifiedAccessAttribute>]
+module Dialog
+
+open System
+
+type Dialog = Widget list
+and Widget = 
+    | Title of string
+    | Label of string
+    | Menu of string * (Item list)
+and Item = 
+    | Item of char * string * string
+    
+type DecoratedText = {
+    Text : string;
+    BGColor : ConsoleColor;
+    FGColor : ConsoleColor;
+} 
+
+let newDecoratedText text bg fg = { Text = text; BGColor = bg; FGColor = fg }
+
+type decoratedTexts = DecoratedText list        
+
