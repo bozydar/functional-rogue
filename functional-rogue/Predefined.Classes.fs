83e6d38ff0b53ae4a06719f3988aa7acd6128b09
Bozydar Sobczak <bozysob@gmail.com>
2012-04-08 15:41:37 +0200

    Resources: Infrastructure for character classes
    --HG--
    branch : mechanics

---
+++ functional-rogue/Predefined.Classes.fs
@@ -0,0 +1,15 @@
+﻿[<RequireQualifiedAccessAttribute>]
+module Predefined.Classes
+
+open Microsoft.FSharp.Reflection
+
+type PlayerClass = 
+    | Navigator
+    | Soldier
+    | Medic
+    | Engineer
+
+let getClasses = 
+    let unionCaseInfos = FSharpType.GetUnionCases(typeof<PlayerClass>)
+    unionCaseInfos |> Array.map (fun item -> item.Name)
+
