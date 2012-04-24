f2351803f2072daa6377c676ef3beddae844a5e5
Bozydar Sobczak <bozysob@gmail.com>
2012-04-24 14:35:57 +0200

    Refactor: Keyboard module added
    --HG--
    branch : items

---
+++ functional-rogue/Keyboard.fs
@@ -0,0 +1,13 @@
+﻿[<AutoOpen>]
+module Keyboard
+
+open System
+
+let (|Key|_|) (charOrConsoleKey : obj) (input : ConsoleKeyInfo) =
+    match charOrConsoleKey with
+    | :? char as thisChar when thisChar = input.KeyChar -> Some()
+    | :? ConsoleKey as thisKey when thisKey = input.Key -> Some()
+    | _ -> None
+
+let (|Keys|_|) (charsOrConsoleKeys : seq<obj>) (input : ConsoleKeyInfo) =
+    if charsOrConsoleKeys |> Seq.exists (fun item -> match input with | Key item -> true | _ -> false) then Some() else None
