10f5a8f6b363b3a4786b75b906dbeea3660a3f86
Bozydar Sobczak <bozysob@gmail.com>
2012-05-08 07:11:43 +0200

    Dialog: Dialog builder hadles text decoration now
    --HG--
    branch : feature/DT-refactor

--- functional-rogue/Dialog.fs
+++ functional-rogue/Dialog.fs
@@ -1,4 +1,5 @@
-﻿module Dialog
+﻿[<RequireQualifiedAccessAttribute>]
+module Dialog
 
 open System
 
@@ -21,64 +22,6 @@ type DecoratedText = {
     FGColor : ConsoleColor;
 }
 
-type BG = { Color : ConsoleColor }
-    with 
-        static member Black = { Color = ConsoleColor.Black}
-        static member Blue = { Color = ConsoleColor.Blue}
-        static member Cyan = { Color = ConsoleColor.Cyan}
-        static member DarkBlue = { Color = ConsoleColor.DarkBlue}
-        static member DarkCyan = { Color = ConsoleColor.DarkCyan}
-        static member DarkGreen = { Color = ConsoleColor.DarkGreen}
-        static member DarkMagenta = { Color = ConsoleColor.DarkMagenta}
-        static member DarkRed = { Color = ConsoleColor.DarkRed}
-        static member DarkYellow = { Color = ConsoleColor.DarkYellow}
-        static member Gray = { Color = ConsoleColor.Gray}
-        static member Green = { Color = ConsoleColor.Green}
-        static member Magenta = { Color = ConsoleColor.Magenta}
-        static member Red = { Color = ConsoleColor.Red}
-        static member White = { Color = ConsoleColor.White}
-        static member Yellow = { Color = ConsoleColor.Yellow}
-
-type FG = { Color : ConsoleColor }
-    with 
-        static member Black = { Color = ConsoleColor.Black}
-        static member Blue = { Color = ConsoleColor.Blue}
-        static member Cyan = { Color = ConsoleColor.Cyan}
-        static member DarkBlue = { Color = ConsoleColor.DarkBlue}
-        static member DarkCyan = { Color = ConsoleColor.DarkCyan}
-        static member DarkGreen = { Color = ConsoleColor.DarkGreen}
-        static member DarkMagenta = { Color = ConsoleColor.DarkMagenta}
-        static member DarkRed = { Color = ConsoleColor.DarkRed}
-        static member DarkYellow = { Color = ConsoleColor.DarkYellow}
-        static member Gray = { Color = ConsoleColor.Gray}
-        static member Green = { Color = ConsoleColor.Green}
-        static member Magenta = { Color = ConsoleColor.Magenta}
-        static member Red = { Color = ConsoleColor.Red}
-        static member White = { Color = ConsoleColor.White}
-        static member Yellow = { Color = ConsoleColor.Yellow}
-
-
-
-type DecoratedTextBuilder (text : string, background : BG, foreground : FG) = 
-    member this.Text = text
-    member this.Background = background
-    member this.Foreground = foreground
-
-    new() = DecoratedTextBuilder("", BG.Black, FG.Gray)
-        
-
-    static member (=>) (me : DecoratedTextBuilder, text : string) =
-        DecoratedTextBuilder(text, me.Background, me.Foreground)
-
-    static member (=>) (me : DecoratedTextBuilder, foreground : FG) =
-        DecoratedTextBuilder(me.Text, me.Background, foreground)
-
-    static member (=>) (me : DecoratedTextBuilder, background : BG) =
-        DecoratedTextBuilder(me.Text, background, me.Foreground)
-
-    member this.ToDecoratedText = { Text = this.Text; BGColor = this.Background.Color; FGColor = this.Foreground.Color }
-
-
 let newDecoratedText text bg fg = { Text = text; BGColor = bg; FGColor = fg }
 
 type Dialog (sequence : seq<Widget>) = 
@@ -88,8 +31,13 @@ type Dialog (sequence : seq<Widget>) =
         member this.GetEnumerator () : System.Collections.IEnumerator  =
             (sequence :> System.Collections.IEnumerable).GetEnumerator ()
 
-    with static member (+) (left : Dialog, right : Dialog) = new Dialog (Seq.append left right)
+    with 
+        static member (+) (left : Dialog, right : Dialog) = new Dialog (Seq.append left right)
+
+
+
 and Widget = 
+    | Nothing
     | CR
     | Title of string
     | Label of string
@@ -98,6 +46,7 @@ and Widget =
     | Subdialog of Input * string * Dialog
     | Raw of DecoratedText
     | Textbox of Input * string
+   
 with static member newDecoratedText (text, bg, fg) =
         Widget.Raw(newDecoratedText text bg fg)
 and OptionItem = string * string
