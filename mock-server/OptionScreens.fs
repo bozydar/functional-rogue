5fec3fed3fb3eace2fbf42973a62a7981ee5164e
Bozydar Sobczak <bozysob@gmail.com>
2012-07-30 16:21:30 +0200

    Keyboard events
    --HG--
    branch : xna

--- mock-server/OptionScreens.fs
+++ mock-server/OptionScreens.fs
@@ -7,16 +7,15 @@ type MainMenu(client : IClient, server : IServer, screenManager : IScreenManager
     inherit Screen()
 
     override this.CreateChildren () =
-        this.Gui.Widgets <- [| 
+        [| 
             Button(10, 30, "New Game", 2, this.NewGame)
             Button(10, 60, "Options", 2, this.ShowOptions)
             Button(10, 90, "Exit", 2, this.Exit)
         |]
-        this.Gui.KeyDown.AddHandler(
-            fun sender keyEventArgs -> 
-                match keyEventArgs.KeyCode with 
-                | Input.Keys.N -> this.NewGame (sender :?> Widget)
-                | _ -> () )
+    override this.OnKeyDown _ e =
+        match e.KeyCode with 
+        | Input.Keys.N -> this.NewGame null
+        | _ -> ()
 
     member private this.Exit _ =
         client.Exit()
@@ -24,14 +23,14 @@ type MainMenu(client : IClient, server : IServer, screenManager : IScreenManager
     member private this.ShowOptions _ =
         screenManager.Switch ScreenManagerState.OptionsMenu
 
-    member private this.NewGame _ =
+    member private this.NewGame (_ : obj) =
         screenManager.Switch ScreenManagerState.BoardScreen
 
 and OptionsMenu(back) =
     inherit Screen()
 
     override this.CreateChildren () =
-        this.Gui.Widgets <- [| 
+        [| 
             Button(10, 30, "Option 1", 2, (fun (x : Widget) -> (x :?> Button).Label <- "oo 1"))
             Button(10, 60, "Option 2", 2, (fun (x : Widget) -> (x :?> Button).Label <- "oo 2"))
             Button(10, 90, "Option 3", 2, (fun (x : Widget) -> (x :?> Button).Label <- "oo 3"))
